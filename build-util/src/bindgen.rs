c3b824fc7ecae79eac0c69d8ec9fae13890f5c6c
Aphek <bilkow@tutanota.com>
2023-12-22 18:30:30 -0300

    Group foreign items by feature and refactor

--- build-util/src/bindgen.rs
+++ build-util/src/bindgen.rs
@@ -1,12 +1,9 @@
 use std::{env, fs, io, path::Path, process};
 
 use quote::ToTokens;
-use syn::{ForeignItem, Item, Type};
+use syn::{self, visit_mut::VisitMut};
 
-use crate::link_visitor::{
-    syn::{self, visit_mut::VisitMut},
-    Link,
-};
+use crate::visitors::{Link, Sort};
 
 pub fn generate(
     vita_headers_include: &Path,
@@ -25,11 +22,12 @@ pub fn generate(
         "Loading vita-headers metadata yaml files from \"{}\"",
         db.to_string_lossy()
     );
-    let mut link = Link::load(db, vitasdk_sys_manifest);
-    link.visit_file_mut(&mut bindings);
+    let link = Link::load(db, vitasdk_sys_manifest);
+
+    link.visit(&mut bindings);
 
     // We sort items here so generated bindings don't depend on the included order.
-    sort_items(&mut bindings.items);
+    Sort.visit_file_mut(&mut bindings);
 
     let bindings = bindings.into_token_stream();
 
@@ -96,98 +94,4 @@ fn generate_preprocessed_bindings(
     };
 
     builder.generate().expect("bindgen failed").to_string()
-}
-
-/// Sorts items on alphabetical order based on normalized identifier.
-/// Bindgen items will be moved to the start.
-fn sort_items(items: &mut [Item]) {
-    items.sort_by_cached_key(|item| {
-        let (precedence, ident) =
-            match item {
-                Item::ExternCrate(i) => (0, i.ident.to_string()),
-                Item::Use(_i) => (1, String::new()),
-                Item::Mod(i) => (2, i.ident.to_string()),
-                Item::Macro(i) => (
-                    3,
-                    i.ident
-                        .as_ref()
-                        .map(|i| i.to_string())
-                        .unwrap_or_else(String::new),
-                ),
-                Item::Static(i) => (4, i.ident.to_string()),
-                Item::Const(i) => (4, i.ident.to_string()),
-                Item::Trait(i) => (4, i.ident.to_string()),
-                Item::TraitAlias(i) => (4, i.ident.to_string()),
-                Item::Type(i) => (4, i.ident.to_string()),
-                Item::Enum(i) => (4, i.ident.to_string()),
-                Item::Struct(i) => (4, i.ident.to_string()),
-                Item::Impl(i) => {
-                    let ident =
-                        match &*i.self_ty {
-                            Type::Path(path_type) => path_type.path.segments.iter().fold(
-                                String::new(),
-                                |acc, segment| {
-                                    let ident_string = segment.ident.to_string();
-                                    if acc.is_empty() {
-                                        ident_string
-                                    } else {
-                                        acc + "::" + &ident_string
-                                    }
-                                },
-                            ),
-                            ty => {
-                                log::warn!("impl on unexpected type {ty:?}");
-                                String::new()
-                            }
-                        };
-                    (4, ident)
-                }
-                Item::Fn(i) => (4, i.sig.ident.to_string()),
-                Item::ForeignMod(i) => {
-                    // For `extern` blocks, we use the first item's identifier.
-                    let ident = match i.items.len() {
-                        // Could use link name here. But it's not strictly necessary as they're
-                        // already sorted.
-                        0 => String::new(),
-                        len => {
-                            if len > 1 {
-                                log::warn!("Unexpected ForeignMod with multiple items: {i:?}");
-                            }
-
-                            match &i.items[0] {
-                                ForeignItem::Fn(i) => i.sig.ident.to_string(),
-                                ForeignItem::Static(i) => i.ident.to_string(),
-                                ForeignItem::Type(i) => i.ident.to_string(),
-                                i => {
-                                    log::warn!("Unexpected item in foreign mod: {i:?}");
-                                    String::new()
-                                }
-                            }
-                        }
-                    };
-                    (4, ident)
-                }
-                Item::Union(i) => (4, i.ident.to_string()),
-                i => {
-                    log::warn!("Unexpected item: {i:?}");
-                    (10, String::new())
-                }
-            };
-        consider_bindgen((precedence, normalize_str(&ident)))
-    });
-}
-
-fn normalize_str(input: &str) -> String {
-    input.to_lowercase().replace('_', "")
-}
-
-fn consider_bindgen(keys: (i32, String)) -> (i32, String) {
-    let (precedence, ident) = keys;
-    let new_precedence = if ident.starts_with("bindgen") {
-        // Move bindgen items to the start of the file
-        precedence - 16
-    } else {
-        precedence
-    };
-    (new_precedence, ident)
 }
